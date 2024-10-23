ff863dd1e6fd24a4010f91ae1ea4cc3a4e16b898
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2024-10-23 18:36:52 -0400

    Minor refactor.

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
-//Kneuron extension by Cortex R&D Inc.
+// Kneuron extension by Cortex R&D Inc.
 
-//Hide tooltip popup.
 function injectCSS(css) {
     const style = document.createElement('style');
     style.textContent = css;
     document.head.appendChild(style);
 }
+
 const css = `
 [data-tippy-root]:not(:has(.kn-help-tip)) {
     display: none !important;
@@ -34,33 +34,28 @@ function highlightAndClick(element) {
     }, 300);
 }
 
-//Handle keyboard events.
+// Handle keyboard events.
 document.addEventListener('keydown', function (event) {
+    let element;
+
     if (event.key === 'Enter') {
-        const saveOrSubmitButton = document.querySelector('.kn-submit button') || document.querySelector('a.save');
-        highlightAndClick(saveOrSubmitButton);
+        element = document.querySelector('.kn-submit button') || document.querySelector('a.save');
     } else if (event.key === 'Escape') {
-        const closeOrCancelButton = document.querySelector('.modal_close') || document.querySelector('a.cancel');
-        highlightAndClick(closeOrCancelButton);
+        element = document.querySelector('.modal_close') || document.querySelector('a.cancel');
     } else if (event.altKey) {
         if (event.key >= 1 && event.key <= 6) {
             let pageIndex = Number(event.key);
-            if (pageIndex === 3) //Invert 3 and 4 since Pages is used so much more often.
-                pageIndex = 4;
-            else if (pageIndex === 4)
-                pageIndex = 3;
-            const navElement = document.querySelector(`#sidebar-nav li:nth-child(${pageIndex}) a`);
-            highlightAndClick(navElement);
+            if (pageIndex === 3 || pageIndex === 4) pageIndex = 7 - pageIndex; //Invert 3 and 4 since Pages is used much more often.
+            element = document.querySelector(`#sidebar-nav li:nth-child(${pageIndex}) a`);
         } else if (event.key === '`') {
-            const backButton = document.querySelector(`.toolbox-back`) || document.querySelector(`.ast-button`);
-            highlightAndClick(backButton);
-        } else if (`qaz`.includes(event.key)) {
+            element = document.querySelector('.toolbox-back') || document.querySelector('.ast-button');
+        } else if ('qaz'.includes(event.key)) {
             let toolIndex = 'qaz'.indexOf(event.key) + 1;
-            const toolElement = document.querySelector(`.toolboxLinks li:nth-child(${toolIndex}) a`);
-            highlightAndClick(toolElement);
+            element = document.querySelector(`.toolboxLinks li:nth-child(${toolIndex}) a`);
         } else if (event.key === 's') {
-            const viewSettings = document.querySelector(`.is-active a.settings`);
-            highlightAndClick(viewSettings);
+            element = document.querySelector('.is-active a.settings');
         }
     }
+
+    if (element) highlightAndClick(element);
 });
